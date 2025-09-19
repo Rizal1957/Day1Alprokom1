c032ce525bb3a7dfd1a3abae12a905392b562cf1
Rizal1957 <mrizalkafidalloh@gmail.com>
2025-09-19 20:16:33 +0700

    Add files via upload

---
+++ ApaYaa.java
@@ -0,0 +1,12 @@
+package apayaa;
+
+public class ApaYaa {
+    public static void main(String[] args) {
+        System.out.print("Nama           : ");
+        System.out.println("Muhammad Rizal Kafidalloh");
+        System.out.print("Nama Panggilan : ");
+        System.out.println("Rizal");
+        System.out.print("Daerah Asal    : ");
+        System.out.println("Krian, Sidoarjo ");
+    }
+}
---
+++ TugasDay1.java
@@ -0,0 +1,25 @@
+package tugasday1;
+public class TugasDay1 {
+    public static void main(String[] args) {
+       System.out.print("Nama           : ");
+        System.out.println("Muhammad Rizal Kafidalloh");
+        System.out.print("Nama Panggilan : ");
+        System.out.println("Rizal");
+        System.out.print("NRP            : ");
+        System.out.println("5002251001");
+        System.out.print("Departemen     : ");
+        System.out.println("Matematika");
+        System.out.print("Fakultas       : ");
+        System.out.println("Fakultas Sains dan Analitika Data");
+        System.out.print("Kampus         : ");
+        System.out.println("Institut Teknologi SepuluH Nopember");
+        System.out.print("Asal           : ");
+        System.out.println("Sidoarjo");
+        System.out.print("Sekolah asal   :");
+        System.out.println(" SMA bukan negeri");
+        System.out.print("Hobi           : ");
+        System.out.println("Bulutangkis");
+        System.out.print("Fun Fact       : ");
+        System.out.println("Suka makan pedes ");
+    }
+}
